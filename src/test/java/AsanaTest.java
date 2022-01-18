f882526239bed05089c21d652b152ab6a83548f3
kpetkowska <kingapetkowska@gmail.com>
2022-01-18 13:09:07 +0100

    request specification & respond specification

--- src/test/java/AsanaTest.java
+++ src/test/java/AsanaTest.java
@@ -1,14 +1,314 @@
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import models.DataStore;
+import models.Student;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.when;
+import java.io.File;
+import java.util.HashMap;
+import java.util.regex.Matcher;
 
-public class AsanaTest extends TestBase {
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class AsanaTest {
+
+    HashMap<String, String> studentMap = new HashMap<>();
+    Student student;
+    float speedWind;
 
     @Test
-    public void shouldGetWorkspaces(){
+    public void shouldGetWorkspaces() {
         when()
                 .get("https://app.asana.com/api/1.0/workspaces").
         then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void postNewStudent() {
+        String requestBody =
+                "{\n" +
+                        "    \"first_name\": \"Angelina\",\n" +
+                        "    \"middle_name\": \"Jolie\",\n" +
+                        "    \"last_name\": \"Camila\",\n" +
+                        "    \"date_of_birth\": \"01/03/1977\"\n" +
+                        "}";
+
+        given()
+                .baseUri("http://www.thetestingworldapi.com/")
+                .basePath("api/studentsDetails")
+                .contentType(ContentType.JSON)
+                .log()
+                .body().
+        when()
+                .body(requestBody)
+                .post().
+        then()
+                .log()
+                .all()
+                .statusCode(201);
+    }
+
+    @Test
+    public void postAnotherStudent() {
+
+        studentMap.put("first_name","Brad");
+        studentMap.put("middle_name","Janusz");
+        studentMap.put("last_name","Pitt");
+        studentMap.put("date_of_birth","01/03/1977");
+
+        given()
+                .baseUri("http://www.thetestingworldapi.com/")
+                .basePath("api/studentsDetails")
+                .contentType(ContentType.JSON)
+                .log()
+                .body().
+        when()
+                .body(studentMap)
+                .post().
+        then()
+                .log()
+                .all()
+                .statusCode(201);
+    }
+
+    @Test
+    public void postStudent() {
+        student = new Student("Salma", "Joanna", "Hayek", "01/03/1977");
+
+        given()
+                .baseUri("http://www.thetestingworldapi.com/")
+                .basePath("api/studentsDetails")
+                .contentType(ContentType.JSON)
+                .log()
+                .body().
+        when()
+                .body(student)
+                .post().
+        then()
+                .log()
+                .all()
+                .statusCode(201);
+    }
+
+    @Test
+    public void postOMGANOTHERStudent() {
+
+        given()
+                .baseUri("http://www.thetestingworldapi.com/")
+                .basePath("api/studentsDetails")
+                .contentType(ContentType.JSON)
+                .log()
+                .body().
+        when()
+                .body(new File("src/test/resources/student.json"))
+                .post()
+                .then()
+                .log()
+                .all()
+                .statusCode(201);
+    }
+
+    @Test
+    public void requestSpec() {
+        given()
+                .param("q","London,uk")
+                .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                .log()
+                .all().
+        when()
+                .get("https://samples.openweathermap.org/data/2.5/weather")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void requestSpecWithObject() {
+
+        RequestSpecification requestSpec =
+                given()
+                        .header("name", "Kinga")
+                        .param("q","London,uk")
+                        .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                        .log()
+                        .all();
+
+        given()
+                .spec(requestSpec).
+        when()
+                .get("https://samples.openweathermap.org/data/2.5/weather")
+                .then()
+                .log()
+                .all()
+                .statusCode(200);
+    }
+
+    @Test
+    public void responseSpecWithObject() {
+
+        ResponseSpecification responseSpec = RestAssured.expect();
+
+        responseSpec
+                .log()
+                .all()
+                .time(Matchers.lessThan(5000L))
+                .contentType(ContentType.JSON)
+                .statusCode(200);
+
+        given()
+                .param("q","London,uk")
+                .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                .log()
+                .all().
+        when()
+                .get("https://samples.openweathermap.org/data/2.5/weather").
+        then()
+                .spec(responseSpec);
+    }
+
+    @Test
+    public void getWindSpeed() {
+        RequestSpecification requestSpec =
+                given()
+                        .header("name", "Kinga")
+                        .param("q","London,uk")
+                        .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                        .log()
+                        .all();
+
+
+        DataStore.WINDSPEED = given()
+                .param("q","London,uk")
+                .param("appid", "b1b15e88fa797225412429c1c50c122a1").
+        when()
+                .get("https://samples.openweathermap.org/data/2.5/weather").
+        then()
+                .log()
+                .all()
+                .extract()
+                .path("wind.speed");
+
+        System.out.println("Speed wind = " + DataStore.WINDSPEED);
+    }
+
+    @Test
+    public void getCountry() {
+        RequestSpecification requestSpec =
+                given()
+                        .header("name", "Kinga")
+                        .param("q","London,uk")
+                        .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                        .log()
+                        .all();
+
+
+        DataStore.COUNTRY = given()
+                .spec(requestSpec).
+                when()
+                    .get("https://samples.openweathermap.org/data/2.5/weather").
+//                  .post("https://samples.openweathermap.org/data/2.5/weather" + DataStore.COUNTRY + "/country")
+                then()
+                    .log()
+                    .all()
+                    .extract()
+                    .path("sys.country");
+
+        System.out.println("Country is:  " + DataStore.COUNTRY);
+    }
+
+    @Test
+    public void isWindSpeedCorrect() {
+        RequestSpecification requestSpec = given()
+                .header("name", "Kinga")
+                .param("q","London,uk")
+                .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                .log()
+                .all();
+
+
+        given()
+                .spec(requestSpec).
+                when()
+                .get("https://samples.openweathermap.org/data/2.5/weather").
+        then()
+                .log()
+                .all()
+                .body("wind.speed", is(4.1f))
+                .statusCode(200);
+
+        System.out.println("Wind speed is:  " + DataStore.WINDSPEED);
+    }
+
+    @Test
+    public void isNameCorrect() {
+        RequestSpecification requestSpec = given()
+                .header("name", "Kinga")
+                .param("q","London,uk")
+                .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                .log()
+                .all();
+
+
+        given()
+                .spec(requestSpec).
+                when()
+                .get("https://samples.openweathermap.org/data/2.5/weather").
+                then()
+                .log()
+                .all()
+                .body("name", equalTo("London"))
+                .statusCode(200);
+    }
+
+    @Test
+    public void isWindSpeedCorrectNotKnowingValue() {
+        RequestSpecification requestSpec = given()
+                .header("name", "Kinga")
+                .param("q","London,uk")
+                .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                .log()
+                .all();
+
+
+        given()
+                .spec(requestSpec).
+        when()
+                .get("https://samples.openweathermap.org/data/2.5/weather").
+        then()
+                .log()
+                .all()
+                .body("wind.speed", greaterThan(4.0f))
+                .statusCode(200);
+    }
+
+    @Test
+    public void isCountryCorrectInMap() {
+        RequestSpecification requestSpec = given()
+                .header("name", "Kinga")
+                .param("q","London,uk")
+                .param("appid", "b1b15e88fa797225412429c1c50c122a1")
+                .log()
+                .all();
+
+
+        given()
+                .spec(requestSpec).
+        when()
+                .get("https://samples.openweathermap.org/data/2.5/weather").
+        then()
+                .log()
+                .all()
+//                .body("sys", hasEntry("country", "GB"))
+//                .body("sys", empty()) when map is empty
+                .body("sys", hasValue("GB")) // it is possible to validate more than one options (ca be 2 bodies)
+                .body("name", equalTo("London"))
                 .statusCode(200);
     }
 }
--- src/test/java/JsonPlaceholder.java
+++ src/test/java/JsonPlaceholder.java
@@ -1,5 +1,6 @@
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -66,5 +67,121 @@ public class JsonPlaceholder extends TestBase {
         assertEquals("Bret", json.get("username"));
         assertEquals("Leanne Graham", json.get("name"));
         assertEquals("Sincere@april.biz", json.get("email"));
+    }
+
+    @Test
+    public void shouldDeleteFirstUser() {
+        given()
+                .when()
+                .delete(BASE_URL + USERS + "/1")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    public void shouldCreateNewUser() {
+        String user = "{\n"  +
+                "    \"name\": \"Kinga Petko\",\n" +
+                "    \"username\": \"xxxxxxx\",\n" +
+                "    \"email\": \"newEmail@april.biz\",\n" +
+                "    \"address\": {\n" +
+                "      \"street\": \"Kulas Light\",\n" +
+                "      \"suite\": \"Apt. 556\",\n" +
+                "      \"city\": \"Gwenborough\",\n" +
+                "      \"zipcode\": \"92998-3874\",\n" +
+                "      \"geo\": {\n" +
+                "        \"lat\": \"-37.3159\",\n" +
+                "        \"lng\": \"81.1496\"\n" +
+                "      }\n" +
+                "    },\n" +
+                "    \"phone\": \"1-770-736-8031 x56442\",\n" +
+                "    \"website\": \"hildegard.org\",\n" +
+                "    \"company\": {\n" +
+                "      \"name\": \"Romaguera-Crona\",\n" +
+                "      \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
+                "      \"bs\": \"harness real-time e-markets\"\n" +
+                "    }\n" +
+                "  }";
+
+        Response response =
+        given()
+                .contentType("application/json")
+                .body(user)
+                .when()
+                .post(BASE_URL + USERS)
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+
+        assertEquals("xxxxxxx", json.get("username"));
+        assertEquals("Kinga Petko", json.get("name"));
+    }
+
+    @Test
+    public void shouldUpdateUser() {
+        String user = "{\n" +
+                "    \"name\": \"Kinga Petko\",\n" +
+                "    \"username\": \"xxxxxxx022\",\n" +
+                "    \"email\": \"newestEmail@april.biz\",\n" +
+                "    \"address\": {\n" +
+                "      \"street\": \"Kulas Light\",\n" +
+                "      \"suite\": \"Apt. 556\",\n" +
+                "      \"city\": \"Gwenborough\",\n" +
+                "      \"zipcode\": \"92998-3874\",\n" +
+                "      \"geo\": {\n" +
+                "        \"lat\": \"-37.3159\",\n" +
+                "        \"lng\": \"81.1496\"\n" +
+                "      }\n" +
+                "    },\n" +
+                "    \"phone\": \"1-770-736-8031 x56442\",\n" +
+                "    \"website\": \"hildegard.org\",\n" +
+                "    \"company\": {\n" +
+                "      \"name\": \"Romaguera-Crona\",\n" +
+                "      \"catchPhrase\": \"Multi-layered client-server neural-net\",\n" +
+                "      \"bs\": \"harness real-time e-markets\"\n" +
+                "    }\n" +
+                "  }";
+
+        Response response =
+        given()
+                .contentType("application/json")
+                .body(user)
+                .when()
+                .put(BASE_URL + USERS + "/1")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+
+        JsonPath json = response.jsonPath();
+        assertEquals("xxxxxxx022", json.get("username"));
+        assertEquals("newestEmail@april.biz", json.get("email"));
+    }
+
+    @Test
+    public void shouldPatchUser() {
+//        String user = "{\n" +
+//                "    \"email\": \"Sincere@april.biz\"\n" + "}";
+
+        JSONObject user = new JSONObject();
+        user.put("email", "entirelyNewEmail@april.biz");
+
+        Response response =
+                given()
+                        .contentType("application/json")
+                        .body(user.toString())
+                        .when()
+                        .patch(BASE_URL + USERS + "/1")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        JsonPath json = response.jsonPath();
+
+        assertEquals("entirelyNewEmail@april.biz" , json.get("email"));
     }
 }
