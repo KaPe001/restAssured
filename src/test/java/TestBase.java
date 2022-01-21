ec34c7bf72741f94e4f11e8a8ec7c1d0608debe1
kpetkowska <kingapetkowska@gmail.com>
2022-01-21 17:09:16 +0100

    uncommented line of code

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -8,7 +8,7 @@ public class TestBase {
     @BeforeAll
     public static void setUp(){
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
-//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.authentication = RestAssured.oauth2("1/1201629892164949:d726687d7214eb9fe9e023fb2acd6309");
     }
 }
